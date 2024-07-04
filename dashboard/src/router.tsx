ab9d7bf28aeead527f7cce7048400223d765170b
Sushil Dhakal <sushildhakal1@gmail.com>
2024-07-04 23:03:49 +1000

    add login page

--- dashboard/src/router.tsx
+++ dashboard/src/router.tsx
@@ -1,3 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import LoginPage from "./pages/Login";
+import HomePage from "./pages/HomePage";
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+]);
+
+export default router;
